a140fddc4f06e62f80ae35e7c22457f3ab454eb2
tx-w <7542400+tx-w@users.noreply.github.com>
2025-02-10 23:16:22 -0800

    completed 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) { //takes string and number of times repeated
+    word3 = ""
+    for (i = 0; i < num; i++){
+        word3 += word
+    } 
+    return word3
 };
 
 // Do not edit below this line
