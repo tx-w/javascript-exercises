991c759d780d995150da92e11878f050117f0eb8
tx-w <7542400+tx-w@users.noreply.github.com>
2025-02-11 00:12:50 -0800

    finished 3...

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const myArray = word.split(" ")
+
+    for(i = 0; i < myArray.length; i++){
+        let newWord = "";
+        newWord = myArray[i]
+        let splitArray = newWord.split('')
+        splitArray.reverse();
+        let joinArray = splitArray.join('')
+        myArray[i] = joinArray                
+    }
+
+    myArray.reverse()
+    finishedArray = myArray.join(" ")
+
+    return finishedArray
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//solution 
+//return string.split("").reverse().join("");
